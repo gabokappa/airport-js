d90215d1b822d3ea90a94b037e4a3fc30dce93dc
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-17 20:35:50 +0100

    Following the walkthrough

--- src/Plane.js
+++ src/Plane.js
@@ -20,3 +20,11 @@ function Plane(){}
 // Plane.prototype.land = function(airport) {
 //   airport.hanger.push(this);
 // };
+
+// function Dog(name) {
+//   this.name = name;
+// }
+//
+// Dog.prototype.bark = function() {
+//   console.log(this.name + ' says Woof!')
+// };
